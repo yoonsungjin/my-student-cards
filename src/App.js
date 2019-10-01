511e3fbf554268fa83d9dc74966a0020a65ab8a2
Michael YOON <myoon@plan-group.com>
2019-10-01 15:08:26 -0400

    feat:fetch json complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    cards: []
+  }
+
+  componentDidMount() {
+    fetch('https://www.hatchways.io/api/assessment/students')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ cards: data })
+        console.log(this.state.cards)
+      })
+      .catch(console.log)
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
 }
 
 export default App;
