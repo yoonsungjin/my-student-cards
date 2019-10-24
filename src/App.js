6c73c4c9c838fc8b8a8d29cc6a9f2115aed798ee
Sungjin Yoon <myoon567@gmail.com>
2019-10-24 13:08:51 -0400

    feat

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
     super(props);
     this.state = {
       items: [],
-      isLoaded: false
+      isLoaded: false,
+      query: null,
+      filteredList:
     };
   }
 
@@ -19,29 +21,51 @@ class App extends Component {
         this.setState({
           isLoaded: true,
           items: students,
+          query: null,
+          filteredList: []
         })
       });
   }
-  render() {
-    const { isLoaded, items } = this.state;
-    console.log(items);
-    if (!isLoaded) {
-      return <div>loading data...</div>;
-    }
 
-    else {
+  updateQuery = query => {
+    this.setState({ query: query });
+    if (query.length > 0) this.handleSearch(query);
+    else this.setState({ filteredList: [] });
+  };
 
-      return (
-        <div className="Data">
-          {items.map(item => (
-            <Student item={item} />
-          ))};
+  handleSearch(query) {
+    this.state.items.filter()
+    BooksAPI.search(query.trim(), 20).then(books => {
+      books.error
+        ? this.setState({ filteredBooks: [] })
+        : this.setState({ filteredBooks: books });
+    });
+
+    render() {
+      const { isLoaded, items } = this.state;
+      console.log(items);
+      if (!isLoaded) {
+        return <div>loading data...</div>;
+      }
+
+      else {
+
+        return (
+          <div className="Data">
+            <input
+              type="text"
+              placeholder="Search by name"
+              onChange={event => this.updateQuery(event.target.value)}
+            />
+            {items.map(item => (
+              <Student item={item} />
+            ))};
         </div>
 
-      );
+        );
+      }
+
     }
-
   }
-}
 
-export default App;
+  export default App;
